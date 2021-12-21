2e7e7d34551dadf7e180c5f21d99b7b11a1a10a3
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2021-12-21 17:03:10 +0000

    Fix linters errors

--- script/index.js
+++ script/index.js
@@ -1,61 +1,61 @@
+const MAIN_FEATURED = document.querySelector('.main-featured');  
 const FEATURED_PLAYERS = [
-{
-  Id: '1',
-  Img: './images/mobile/main-page/s1mple.png',
-  Name: 'Aleksandr \'s1mple\' Kostyliev',
-  Role: 'Professional CS:GO player playing for NaVi Team',
-  Description: 's1mple is an Ukrainian profesisonal player, he has won 18 HLTV MVP Medals, a Major and has been crowned as the best player of 2018, 2019 and 2020.',
-},
-{
-  Id: '2',
-  Img: './images/mobile/main-page/TenZ.jpg',
-  Name: 'Tyson \'TenZ\' Ngo',
-  Role: 'Professional VALORANT player playing for Sentinels Team',
-  Description: 'TenZ is a Canadian professional player, he is an aspiring Valorant streamer and a competitor.',
-},
-{
-  Id: '3',
-  Img: './images/mobile/main-page/ImperialHal.jpg',
-  Name: 'Phillip \'ImperialHal\' Dosen',
-  Role: 'Professional Apex Legends player playing for TSM Team',
-  Description: 'ImperialHal is an American professional player, playing Apex Legends since 2019 and winning 10 awards.',
-},
-{
-  Id: '4',
-  Img: './images/mobile/main-page/HusKerrs.jpg',
-  Name: 'Jordan \'HusKerrs\' Thomas',
-  Role: 'Professional COD Warzone player and streamer playing for NRG Team',
-  Description: 'HusKerrs is an American professional player and the first to earn $100K in Warzone.',
-},
-{
-  Id: '5',
-  Img: './images/mobile/main-page/Pengu.png',
-  Name: 'Niclas \'Pengu\' Mouritzen',
-  Role: 'Professional Rainbow Six Siege player playing for G2 Esports',
-  Description: 'Pengu is a Danish professional player, considered by far and away the best player in the world.'
-},
-{
-  Id: '6',
-  Img: './images/mobile/main-page/ZywOo.jpg',
-  Name: 'Mathieu \'ZywOo\' Herbaut',
-  Role: 'Professional CS:GO player playing for Team Vitality',
-  Description: 'ZywOo is a French professional player, he is one of the best CS:GO players in the world.'
-}];
+  {
+    Id: '1',
+    Img: './images/mobile/main-page/s1mple.png',
+    Name: 'Aleksandr \'s1mple\' Kostyliev',
+    Role: 'Professional CS:GO player playing for NaVi Team',
+    Description: 's1mple is an Ukrainian profesisonal player, he has won 18 HLTV MVP Medals, a Major and has been crowned as the best player of 2018, 2019 and 2020.',
+  },
+  {
+    Id: '2',
+    Img: './images/mobile/main-page/TenZ.jpg',
+    Name: 'Tyson \'TenZ\' Ngo',
+    Role: 'Professional VALORANT player playing for Sentinels Team',
+    Description: 'TenZ is a Canadian professional player, he is an aspiring Valorant streamer and a competitor.',
+  },
+  {
+    Id: '3',
+    Img: './images/mobile/main-page/ImperialHal.jpg',
+    Name: 'Phillip \'ImperialHal\' Dosen',
+    Role: 'Professional Apex Legends player playing for TSM Team',
+    Description: 'ImperialHal is an American professional player, playing Apex Legends since 2019 and winning 10 awards.',
+  },
+  {
+    Id: '4',
+    Img: './images/mobile/main-page/HusKerrs.jpg',
+    Name: 'Jordan \'HusKerrs\' Thomas',
+    Role: 'Professional COD Warzone player and streamer playing for NRG Team',
+    Description: 'HusKerrs is an American professional player and the first to earn $100K in Warzone.',
+  },
+  {
+    Id: '5',
+    Img: './images/mobile/main-page/Pengu.png',
+    Name: 'Niclas \'Pengu\' Mouritzen',
+    Role: 'Professional Rainbow Six Siege player playing for G2 Esports',
+    Description: 'Pengu is a Danish professional player, considered by far and away the best player in the world.',
+  },
+  {
+    Id: '6',
+    Img: './images/mobile/main-page/ZywOo.jpg',
+    Name: 'Mathieu \'ZywOo\' Herbaut',
+    Role: 'Professional CS:GO player playing for Team Vitality',
+    Description: 'ZywOo is a French professional player, he is one of the best CS:GO players in the world.',
+  }];
 
-let MORE_BUTTON = document.createElement('button');
+const MORE_BUTTON = document.createElement('button');
 MORE_BUTTON.type = 'button';
 MORE_BUTTON.innerHTML = 'MORE';
 MORE_BUTTON.classList.add('web-button');
 
-let MORE_ICON = document.querySelector('i');
+const MORE_ICON = document.querySelector('i');
 MORE_ICON.classList.add('fa', 'fa-angle-down');
 MORE_BUTTON.appendChild(MORE_ICON);
 
-
-function loadFirstFeatured () {
-  if(screen.width < 768) {
+function loadFirstFeatured() {
+  if (screen.width < 768) {
     for (let i = 0; i < 2; i += 1) {
-    const FIRST_PLAYERS = 
+      const FIRST_PLAYERS = 
     `<div class="featured-player">
       <img src=${FEATURED_PLAYERS[i].Img}>
       <div class="player-description">
@@ -63,8 +63,8 @@ function loadFirstFeatured () {
        <h5>${FEATURED_PLAYERS[i].Role}<hr></h5>
         <p>${FEATURED_PLAYERS[i].Description}</p>
       </div>
-    </div>`
-    MAIN_FEATURED.innerHTML += FIRST_PLAYERS;
+    </div>`;
+      MAIN_FEATURED.innerHTML += FIRST_PLAYERS;
     }
   } else if (screen.width > 768) {
     MORE_BUTTON.style.display = 'none';
@@ -77,8 +77,8 @@ function loadFirstFeatured () {
        <h5>${FEATURED_PLAYERS[i].Role}<hr></h5>
         <p>${FEATURED_PLAYERS[i].Description}</p>
       </div>
-    </div>`
-    MAIN_FEATURED.innerHTML += ALL_PLAYERS;
+    </div>`;
+      MAIN_FEATURED.innerHTML += ALL_PLAYERS;
     }
   }
 }
@@ -88,7 +88,7 @@ MAIN_FEATURED.appendChild(MORE_BUTTON);
 MORE_BUTTON.addEventListener('click', () => {
   MORE_BUTTON.style.display = 'none';
   for (let i = 2; i < FEATURED_PLAYERS.length; i += 1) {
-  const NEXT_PLAYERS = 
+    const NEXT_PLAYERS = 
 `<div class="featured-player" id="${FEATURED_PLAYERS[i].Id}">
   <img src=${FEATURED_PLAYERS[i].Img}>
   <div class="player-description">
@@ -96,7 +96,7 @@ MORE_BUTTON.addEventListener('click', () => {
   <h5>${FEATURED_PLAYERS[i].Role}<hr></h5>
   <p>${FEATURED_PLAYERS[i].Description}</p>
   </div>
-</div>`
-MAIN_FEATURED.innerHTML += NEXT_PLAYERS;
+</div>`;
+    MAIN_FEATURED.innerHTML += NEXT_PLAYERS;
   }
 });
--- script/script.js
+++ script/script.js
@@ -1,8 +1,6 @@
-const NAVIGATION_MENU = document.querySelector('.navigation-menu');
 const NAVIGATION_ICON = document.querySelector('.navigation-icon');
 const NAVIGATION_LIST = document.querySelector('.navigation-list');
 const NAVIGATION_BTN = document.querySelector('.navigation-button');
-const MAIN_FEATURED = document.querySelector('.main-featured');
 const ABOUT_TEXT = document.querySelector('.aboutme-page-content p');
 const LOGO_TEXT = document.querySelector('.logo-page p');
 
@@ -17,9 +15,9 @@ NAVIGATION_BTN.addEventListener('click', () => {
 });
 
 function desktopText() {
-if (screen.width > 768) {
-  ABOUT_TEXT.innerHTML += 'So if you\'re a gamer and engaged in first person shooters games - CS:GO, Valorant, Apex Legends, COD Warzone and R6 Siege - we hope you\'ll consider joining us this year to build the biggest gaming event.';
-  LOGO_TEXT.innerHTML += 'After online public voting and Selection Committee\'s judging process, the logo from Ariel Camus won the prize. After consultation with Mr. Camus we optimized the prize-winning-logo for this year\'s summit as following:';
+  if (screen.width > 768) {
+    ABOUT_TEXT.innerHTML += 'So if you\'re a gamer and engaged in first person shooters games - CS:GO, Valorant, Apex Legends, COD Warzone and R6 Siege - we hope you\'ll consider joining us this year to build the biggest gaming event.';
+    LOGO_TEXT.innerHTML += 'After online public voting and Selection Committee\'s judging process, the logo from Ariel Camus won the prize. After consultation with Mr. Camus we optimized the prize-winning-logo for this year\'s summit as following:';
   }
 }
 desktopText();
