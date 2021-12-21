bf7e59b3e8d20a11e2b8f252ab2ef352ced9e577
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2021-12-21 17:53:00 +0000

    Fix linters errors #9

--- script/index.js
+++ script/index.js
@@ -55,29 +55,13 @@ MORE_BUTTON.appendChild(MORE_ICON);
 function loadFirstFeatured() {
   if (screen.width < 768) {
     for (let i = 0; i < 2; i += 1) {
-      const FIRST_PLAYERS = 
-    `<div class="featured-player">
-      <img src=${FEATURED_PLAYERS[i].Img}>
-      <div class="player-description">
-       <h4>${FEATURED_PLAYERS[i].Name}</h4>
-       <h5>${FEATURED_PLAYERS[i].Role}<hr></h5>
-        <p>${FEATURED_PLAYERS[i].Description}</p>
-      </div>
-    </div>`;
+      const FIRST_PLAYERS =`<div class="featured-player"><img src=${FEATURED_PLAYERS[i].Img}><div class="player-description"><h4>${FEATURED_PLAYERS[i].Name}</h4><h5>${FEATURED_PLAYERS[i].Role}<hr></h5><p>${FEATURED_PLAYERS[i].Description}</p></div></div>`;
       MAIN_FEATURED.innerHTML += FIRST_PLAYERS;
     }
   } else if (screen.width > 768) {
     MORE_BUTTON.style.display = 'none';
     for (let i = 0; i < FEATURED_PLAYERS.length; i += 1) {
-      const ALL_PLAYERS =
-    `<div class="featured-player">
-      <img src=${FEATURED_PLAYERS[i].Img}>
-      <div class="player-description">
-       <h4>${FEATURED_PLAYERS[i].Name}</h4>
-       <h5>${FEATURED_PLAYERS[i].Role}<hr></h5>
-        <p>${FEATURED_PLAYERS[i].Description}</p>
-      </div>
-    </div>`;
+      const ALL_PLAYERS =`<div class="featured-player"><img src=${FEATURED_PLAYERS[i].Img}><div class="player-description"><h4>${FEATURED_PLAYERS[i].Name}</h4><h5>${FEATURED_PLAYERS[i].Role}<hr></h5><p>${FEATURED_PLAYERS[i].Description}</p></div></div>`;
       MAIN_FEATURED.innerHTML += ALL_PLAYERS;
     }
   }
